4887bcc8196bb87271a503199eae2bc6947d2308
mdx97 <mathewhorner123@gmail.com>
2019-04-04 13:00:59 -0500

    Using 0.0.0.0 for host in app.run().

--- api/service.py
+++ api/service.py
@@ -47,5 +47,5 @@ def events():
         return str(ex)
 
 if __name__ == "__main__":
-    port = os.environ['PORT'] if 'PORT' in os.environ else 5000
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
