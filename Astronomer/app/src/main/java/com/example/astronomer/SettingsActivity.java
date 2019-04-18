85f0dffe83e9c8c9e5257712c86942972cd3cdf1
Connor Beshears <connorbeshears@gmail.com>
2019-04-17 19:25:18 -0500

    Settings Persistance
    Settings for events now persist through changing/app closing

--- Astronomer/app/src/main/java/com/example/astronomer/SettingsActivity.java
+++ Astronomer/app/src/main/java/com/example/astronomer/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.example.astronomer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import java.util.Objects;
@@ -15,22 +19,138 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent ini = getIntent();
+        final Intent ini = getIntent();
         String eventName = ini.getStringExtra("eventName");
 
         setContentView(R.layout.activity_settings);
         ActionBar actionBar = Objects.requireNonNull(getSupportActionBar());
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(getString(R.string.label_settings, eventName));
+
+
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        final SharedPreferences.Editor edit = sp.edit();
+        final Switch hourNoticeSwitch = findViewById(R.id.hourNoticeSwitch);
+        hourNoticeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String strdata = ini.getStringExtra("eventName");
+                strdata = strdata + "hourNotice";
+                if(isChecked){
+                    edit.putBoolean(strdata, true);
+                    Log.d("Storage", "Applied hour status");
+                }
+                else {
+                    edit.putBoolean(strdata, false);
+                    Log.d("Storage", "Disabled hour status");
+
+                }
+                edit.apply();
+            }
+        });
+
+        Switch dayNoticeSwitch = findViewById(R.id.dayNoticeSwitch);
+        dayNoticeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String strdata = ini.getStringExtra("eventName");
+                strdata = strdata + "dayNotice";
+                if(isChecked){
+                    edit.putBoolean(strdata, true);
+                    Log.d("Storage", "Applied day status");
+                }
+                else {
+                    edit.putBoolean(strdata, false);
+                    Log.d("Storage", "Disabled day status");
+
+                }
+                edit.apply();
+            }
+        });
+
+        Switch weekNoticeSwitch = findViewById(R.id.weekNoticeSwitch);
+        weekNoticeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String strdata = ini.getStringExtra("eventName");
+                strdata = strdata + "weekNotice";
+                if(isChecked){
+                    edit.putBoolean(strdata, true);
+                    Log.d("Storage", "Applied week status");
+                }
+                else {
+                    edit.putBoolean(strdata, false);
+                    Log.d("Storage", "Disabled week status");
+
+                }
+                edit.apply();
+            }
+        });
+
+        Switch notificationsSwitch = findViewById(R.id.notificationsSwitch);
+        notificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String strdata = ini.getStringExtra("eventName");
+                strdata = strdata + "notiNotice";
+                if(isChecked){
+                    edit.putBoolean(strdata, true);
+                    Log.d("Storage", "Applied notification status");
+                }
+                else {
+                    edit.putBoolean(strdata, false);
+                    Log.d("Storage", "Disabled notification status");
+
+                }
+                edit.apply();
+            }
+        });
+
+        Switch trackSwitch = findViewById(R.id.trackVisibleOnlySwitch);
+        trackSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String strdata = ini.getStringExtra("eventName");
+                strdata = strdata + "trackNotice";
+                if(isChecked){
+                    edit.putBoolean(strdata, true);
+                    Log.d("Storage", "Applied tracker status");
+                }
+                else {
+                    edit.putBoolean(strdata, false);
+                    Log.d("Storage", "Disabled tracker status");
+
+                }
+                edit.apply();
+            }
+        });
+
+        String strData = ini.getStringExtra("eventName");
+        boolean hr = sp.getBoolean(strData + "hourNotice", false);
+        boolean day = sp.getBoolean(strData + "dayNotice", false);
+        boolean week = sp.getBoolean(strData + "weekNotice", false);
+        boolean noti = sp.getBoolean(strData + "notiNotice", false);
+        boolean track = sp.getBoolean(strData + "trackNotice", false);
+        hourNoticeSwitch.setChecked(hr);
+        dayNoticeSwitch.setChecked(day);
+        weekNoticeSwitch.setChecked(week);
+        notificationsSwitch.setChecked(noti);
+        trackSwitch.setChecked(track);
     }
 
     public void noticesToggle(View v){
         Switch hourNoticeSwitch = findViewById(R.id.hourNoticeSwitch);
         Switch dayNoticeSwitch = findViewById(R.id.dayNoticeSwitch);
         Switch weekNoticeSwitch = findViewById(R.id.weekNoticeSwitch);
-        hourNoticeSwitch.setChecked(false);
-        dayNoticeSwitch.setChecked(false);
-        weekNoticeSwitch.setChecked(false);
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        boolean hr = sp.getBoolean("hourNotice", false);
+        boolean day = sp.getBoolean("dayNotice", false);
+        boolean week = sp.getBoolean("weekNotice", false);
+        hourNoticeSwitch.setChecked(hr);
+        dayNoticeSwitch.setChecked(day);
+        weekNoticeSwitch.setChecked(week);
+
+
     }
 
     public void subNoticesToggle(View v){
@@ -47,7 +167,8 @@ public class SettingsActivity extends AppCompatActivity {
             finish();
             return true;
         }
-        else
+        else {
             return super.onOptionsItemSelected(item);
+        }
     }
 }
