11de48e1ead829f905a0aed4ec3b0eef5df91cb0
mdx97 <mathewhorner123@gmail.com>
2019-05-01 10:51:54 -0500

    Reverted to using local timezone to format sunrise and sunset times.

--- api/sunrise_sunset.py
+++ api/sunrise_sunset.py
@@ -2,6 +2,7 @@ import requests
 import json
 from datetime import datetime, timedelta
 from utils import date_to_string
+import math
 
 def zero_padded_time(time_string):
     if len(time_string) == 10:
@@ -16,4 +17,7 @@ def get_sun_transition_time(transition, date, latitude, longitude):
         raise Exception("Error: no results found!")
     time_string = zero_padded_time(data["results"][transition])
     time = datetime.strptime(time_string, "%I:%M:%S %p")
+    modifier = math.floor(abs(longitude) / 15) * (longitude / (abs(longitude)))
+    time += timedelta(hours=modifier)
+    print(modifier)
     return time.strftime("%I:%M:%S %p")
